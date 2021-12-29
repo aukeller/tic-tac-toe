d9ad3d77808d763c12606ee61a79665bf3575390
Austin Keller <austinkllr@gmail.com>
2021-12-28 21:44:01 -0800

    finished logic for checking win

--- game.js
+++ game.js
@@ -40,7 +40,43 @@ const gamePlayController = (() => {
         currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
    }
 
-    return { getCurrentPlayerSymbol, switchTurn };
+   function checkHorizontalWin(board, currentSymbol) {
+       for (let i = 0; i <= 6; i += 3) {
+           if (board[i] == currentSymbol && board[i + 1] == currentSymbol && board[i + 2] == currentSymbol) {
+               return true;
+           }
+       }
+
+       return false;
+   }
+   
+   function checkVerticalWin(board, currentSymbol) {
+       for (let i = 0; i <= 2; i ++) {
+           if (board[i] == currentSymbol && board[i + 3] == currentSymbol && board[i + 6] == currentSymbol) {
+               return true;
+           }
+       }
+
+       return false;
+   }
+   
+   function checkDiagonalWin(board, currentSymbol) {
+           if (board[0] == currentSymbol && board[4] == currentSymbol && board[8] == currentSymbol) {
+               return true;
+           } else if (board[2] == currentSymbol && board[4] == currentSymbol && board[6] == currentSymbol) {
+               return true;
+           }
+
+       return false;
+   }
+
+   const checkWin = (board, currentSymbol) => {
+       return (checkHorizontalWin(board, currentSymbol) || 
+               checkVerticalWin(board, currentSymbol) || 
+               checkDiagonalWin(board, currentSymbol));
+   }
+
+    return { getCurrentPlayerSymbol, switchTurn, checkWin };
 
 })();
 
@@ -49,30 +85,39 @@ const gamePlayController = (() => {
 const displayController = (() => {
     
     const DOMTiles = document.querySelectorAll('.tile');
-    DOMTiles.forEach(tile => tile.addEventListener('click', (e) => {
-        
-        let currentSymbol = gamePlayController.getCurrentPlayerSymbol();
-        let pos = e.target.id;
 
-        if (e.target.textContent == "") {
-            gameBoard.addMark(currentSymbol, pos);
-            renderContents(gameBoard.board);
-
-            gamePlayController.switchTurn();
-        }
-        
-
-    }));
+    function run() {
+        DOMTiles.forEach(tile => tile.addEventListener('click', (e) => {
+            
+            let currentSymbol = gamePlayController.getCurrentPlayerSymbol();
+            let pos = e.target.id;
     
+            if (e.target.textContent == "") {
+                gameBoard.addMark(currentSymbol, pos);
+                renderContents(gameBoard.board);
     
+                console.log(gamePlayController.checkWin(gameBoard.board, currentSymbol))
+                
+    
+                gamePlayController.switchTurn();
+            }
+            
+    
+        }));
+    }
+    
+
     function renderContents(board) {
         DOMTiles.forEach((tile, index) => {
             tile.textContent = board[index];
         });
     }
 
+    return { run };
+
 })();
 
 
 
 
+displayController.run();
